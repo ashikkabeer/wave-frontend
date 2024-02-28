7448031364c4e03bfc3fd1876795c0f69d1bed07
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-02-28 23:33:12 +0530

    community

--- src/app/circle/page.tsx
+++ src/app/circle/page.tsx
@@ -1,7 +1,7 @@
 import { AnimatedPinDemo } from "@/components/animated-pin";
 export default function Circle() {
   return (
-    <div className="w-screen flex flex-col md:flex-row flex-wrap">
+    <div className="w-screen  flex flex-col md:flex-row flex-wrap">
       <div className="">
         <AnimatedPinDemo />
       </div>
